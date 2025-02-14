c01d5c2307739d11049716deda0b237386951a6e
mmintsdev <mmintsdev@gmail.com>
2025-02-14 17:45:30 +0300

    hw01: Доработка - логгирование

--- src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -1,9 +1,11 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.otus.hw.exceptions.NoQuestionsAvailableException;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class TestRunnerServiceImpl implements TestRunnerService {
 
@@ -25,18 +27,22 @@ public class TestRunnerServiceImpl implements TestRunnerService {
     }
 
     private void handleNoQuestionsAvailableException(NoQuestionsAvailableException e) {
+        log.error("No questions available to execute the test.", e);
         System.out.println("Error: No test questions available.");
     }
 
     private void handleQuestionReadException(QuestionReadException e) {
+        log.error("Error occurred while reading questions from the data source.", e);
         System.out.println("Error when reading questions from the data source.");
     }
 
     private void handleNullPointerException(NullPointerException e) {
+        log.error("A NullPointerException occurred. This might indicate a bug in the code.", e);
         System.out.println("Error: Unexpected error occurred (NullPointerException). Please contact support.");
     }
 
     private void handleUnexpectedException(Exception e) {
-        System.out.println("Error: No test questions available.");
+        log.error("An unexpected error occurred while executing the test.", e);
+        System.out.println("There was an unexpected error: " + e.getMessage());
     }
 }
