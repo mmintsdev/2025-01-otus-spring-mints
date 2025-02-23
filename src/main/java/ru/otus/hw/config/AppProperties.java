8c5775d9afce46da8e9fe2594690e012d9db60a4
mmintsdev <mmintsdev@gmail.com>
2025-02-23 13:24:44 +0300

    hw02: Доработка задачи.

--- src/main/java/ru/otus/hw/config/AppProperties.java
+++ src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,13 +1,20 @@
 package ru.otus.hw.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
-    // внедрить свойство из application.properties
     private int rightAnswersCountToPass;
 
-    // внедрить свойство из application.properties
     private String testFileName;
+
+    private AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+                          @Value("${test.fileName}") String testFileName) {
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
+        this.testFileName = testFileName;
+    }
 }
--- src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,15 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+import java.util.List;
+
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -16,13 +21,34 @@ public class TestServiceImpl implements TestService {
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
+
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question : questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        questions.forEach(question -> {
+            ioService.printLine(question.text());
+            var answersList = question.answers();
+            outputPossibleAnswers(answersList);
+
+            var chosenAnswerIndex = getChosenAnswerIndex(answersList);
+            boolean isAnswerValid = answersList.get(chosenAnswerIndex).isCorrect();
+
+            ioService.printLine(isAnswerValid ? "Correctly!\n" : "Wrong!\n");
             testResult.applyAnswer(question, isAnswerValid);
-        }
+        });
+
         return testResult;
+    }
+
+    private void outputPossibleAnswers(List<Answer> answersList) {
+        answersList.stream()
+                .map(answer -> answersList.indexOf(answer) + 1 + ". " + answer.text())
+                .forEach(ioService::printLine);
+    }
+
+    private int getChosenAnswerIndex(List<Answer> answersList) {
+        ioService.printLine("Enter the response number: ");
+        int chosenAnswerIndex = ioService.readIntForRange(1, answersList.size(), "Incorrect input!") - 1;
+        return chosenAnswerIndex;
     }
 }
